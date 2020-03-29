d80eed708cdff0c47b5a52cd70c3d6504380f83a
Eric Lau <ericlaucmo@gmail.com>
2020-03-29 11:05:12 -0700

    Adjusted proxy to work with user defined endpoints

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ server.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Search bar queries
 server.post('/api/searchbar/', (req, res) => {
-  axios.post('http://18.188.212.111:3100/api/searchbar', req.body)
+  axios
+    .post('http://localhost:3100/api/searchbar', req.body)
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
 });
@@ -21,44 +22,56 @@ server.post('/api/searchbar/', (req, res) => {
 // Search bar history retrieval
 server.get('/api/searchbar/history', (req, res) => {
   axios
-    .get('http://18.188.212.111:3100/api/searchbar/history')
+    .get('http://localhost:3100/api/searchbar/history')
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
 });
 
 // Search bar history insertion
 server.post('/api/searchbar/history', (req, res) => {
-  axios.post('http://18.188.212.111:3100/api/searchbar/history', req.body)
+  axios
+    .post('http://localhost:3100/api/searchbar/history', req.body)
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
 });
 
 // Search bar history clearing
 server.delete('/api/searchbar/history', (req, res) => {
-  axios.delete('http://18.188.212.111:3100/api/searchbar/history')
+  axios
+    .delete('http://localhost:3100/api/searchbar/history')
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
 });
 
 // Category items retrieval
 server.post('/api/navbar/', (req, res) => {
-  axios.post('http://18.188.212.111:3100/api/navbar/', req.body)
+  axios
+    .post('http://localhost:3100/api/navbar/', req.body)
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
 });
 
 // Login authentication
 server.post('/api/login/', (req, res) => {
-  axios.post('http://18.188.212.111:3100/api/login/', req.body)
+  axios
+    .post('http://localhost:3100/api/login/', req.body)
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
 });
 
 // Cart items retrieval
 server.post('/api/cart/', (req, res) => {
-  axios.post('http://18.188.212.111:3100/api/cart/', req.body)
+  axios
+    .post('http://localhost:3100/api/cart/', req.body)
     .then(({ data }) => res.status(200).send(data).end())
     .catch((err) => res.status(400).send(err).end());
+});
+
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => {
+    if (err)
+      res.status(400).send(err).end();
+  });
 });
 
 server.listen(port, () => console.log('Server initialized on port:', port));
